9f2c64ef90cb16462fb2f65786ef14963816978d
ganeshkarrolla2846-lab <ganeshkarrolla2846@gmail.com>
2025-11-15 22:44:20 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useRef, useState } from 'react'
+import './App.css'
+function App() {
+   const OTP_DIGITS_COUNT=5;
+   const [inputArr,setInputArr]=useState(new Array(OTP_DIGITS_COUNT).fill(""));
+   
+   const refArr = useRef([]);
+   useEffect(()=>{
+    refArr.current[0]?.focus();
+   },[])
+   const handleOnChange = (value,index) => 
+   {
+    if(isNaN(value)) return;
+    const newvalue=value.trim();
+    console.log(value);
+    console.log(newvalue);
+    const newArray=[...inputArr];
+    newArray[index]=newvalue.slice(-1);
+    setInputArr(newArray);
+
+    newvalue && refArr.current[index+1]?.focus();
+
+   };
+   const handleOnKeyDown = (e,index) => {
+      if(!e.target.value && e.key==="Backspace")
+        refArr.current[index-1]?.focus();
+   };
+  return (
+    <div className='App'>
+  <h1>Validate OTP</h1>
+  {
+    inputArr.map((input, index) => (
+      <input
+        key={index}
+        className='inputbox'
+        type='text'
+        value={input}
+         ref={(input) => (refArr.current[index]=input)}
+        onChange={(e)=> {handleOnChange(e.target.value,index)}}
+        onKeyDown={(e) => {handleOnKeyDown(e,index)}}
+      />
+    ))
+  }
+</div>
+
+  )
+}
+
+export default App
